29044b1b10b4bae0df7a7430d3d3af7e4413e34d
sambattalio <sambattalio99@gmail.com>
2019-11-09 01:22:29 -0500

    Fixed and updated token

---
+++ backend/__init__.py
@@ -0,0 +1,4 @@
+import sys, os
+
+# ADD BACKEND TO PYTHON PATH
+sys.path.append(os.path.dirname(os.path.realpath(__file__)))
--- backend/token.py
+++ backend/token.py
@@ -3,6 +3,7 @@
 import sqlite3
 import os
 from time import time
+from uuid import uuid1
 
 from constants import DATABASE_FILE
 
@@ -17,6 +18,9 @@ class TokenTable:
 
     TABLE_PATH = os.path.expanduser(DATABASE_PATH)
 
+    SQL_EXISTS = 'SELECT * from Tokens where uuid=? and token=?'
+    SQL_INSERT_POST = 'INSERT INTO Tokens (token, uuid, expire_time) VALUES (?, ?, ?)'
+
     MAX_TTL = 69
 
     def __init__(self):
@@ -27,13 +31,36 @@ class TokenTable:
             curs = self.conn.cursor()
             curs.execute(self.SQL_CREATE_TABLE)
 
-    def _get_epoch_time(self):
+    def _get_epoch_time(self) -> int:
         ''' returns current epoch time '''
         return int(time.time())
 
-    def validate(self, uid, token_id):
+    def _get_ttl(self) -> int:
+        ''' returns time + MAX_TTL constant '''
+        return self._get_epoch_time() + self.MAX_TTL
+
+    def validate(self, uid: str, token_id: str) -> bool:
         ''' Checks if userid has token and the token is valid '''
-        
-        
-    def create(self, uid):
+        self.conn = sqlite3.connect(self.TABLE_PATH)
+
+        with self.conn:
+            curs = self.conn.cursor()
+            # perform query
+            curs.execute(self.SQL_EXISTS, (uid, token_id,))
+            # check if there is a response
+            data = curs.fetchone()
+            if not data: return False
+            # check if time is expired or nah
+            return self._get_epoch_time() < data[2]
+
+    def create(self, uid: str) -> str:
         ''' Create token in table '''
+        self.conn = sqlite3.connect(self.TABLE_PATH)
+
+        with self.conn:
+            curs = self.conn.cursor()
+            # create token
+            tok = str(uuid1())
+            # insert to table
+            curs.execute(self.SQL_INSERT_POST, (tok, uid, self._get_ttl()))
+
