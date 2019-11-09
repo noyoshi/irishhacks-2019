a37257fb770bdf4802bb6e62fe0ef359ad2552db
ginglis <ginglis05@gmail.com>
2019-11-08 22:26:18 -0500

    added create string globs

--- db.py
+++ db.py
@@ -3,26 +3,36 @@ import sqlite3
 
 
 class Database:
+    SQL_CREATE_POST_TABLE = '''CREATE TABLE IF NOT EXISTS Post(
+                post_id int NOT NULL PRIMARY KEY,
+                description TEXT,
+                num_vols INT,
+                location VARCHAR(255),
+                tag1 VARCHAR(50),
+                tag2 VARCHAR(50),
+                tag3 VARCHAR(50),
+                tag4 VARCHAR(50),
+                tag5 VARCHAR(50),
+                type BOOL
+            )'''
+
+    SQL_CREATE_PERSON_TABLE = '''CREATE TABLE IF NOT EXISTS Person(
+                uuid PRIMARY KEY,
+                email VARCHAR(100),
+                phone CHAR(10),
+                name VARCHAR(100),
+                bio TEXT,
+                dob DATE,
+
+            )'''
+
+
     def __init__(self):
         conn = sqlite3.connect('working.db')
         c = conn.cursor()
 
-        #if the count is 1, then table exists
-        c.execute('''SELECT count(name) FROM sqlite_master WHERE type="table" and name="Post"''')
-        if not c.fetchone()[0]:
-            # Post table: type is bool indicating request or offer
-            c.execute('''CREATE TABLE Post(
-                    post_id int NOT NULL PRIMARY KEY,
-                    description text,
-                    num_vols int,
-                    location VARCHAR(255),
-                    tag1 VARCHAR(50),
-                    tag2 VARCHAR(50),
-                    tag3 VARCHAR(50),
-                    tag4 VARCHAR(50),
-                    tag5 VARCHAR(50),
-                    type bool
-                )''')
+        # Post table: type is bool indicating request or offer
+        c.execute()
 
         for row in c.execute('''SELECT * FROM Post'''):
             print(row)
