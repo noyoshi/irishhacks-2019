5d7da5870660d4ad2ce3429f80724085066fdf70
sambattalio <sambattalio99@gmail.com>
2019-11-09 11:29:36 -0500

    half working acc

--- backend/account.py
+++ backend/account.py
@@ -9,60 +9,108 @@ from post import Post
 from typing import List
 
 class Account():
+    '''
+    Abstract class representing general account features
+    '''
 
-    def __init__(self, name, bio=None, email=None, phone=None):
+    DEFAULT_PATH = os.path.expanduser(DATABASE_FILE)
+
+    SQL_SELECT_UUID = 'SELECT * FROM Account WHERE uuid = ?'
+    SQL_INSERT_ACCOUNT = '''INSERT INTO Account VALUES(?, ?, ?, ?, ?, ?, ?)'''
+    SQL_UPDATE_ACCOUNT = '''UPDATE Account SET name=?, email=?, password=?, is_personal=?, bio=?, phone=? WHERE uuid=?'''
+    SQL_DELETE_ACCOUNT = 'DELETE FROM Account WHERE uuid = ?'
+
+    SQL_SELECT_EMAIL = 'SELECT * FROM Account WHERE email = ?'
+    SQL_CHECK_VALID = 'SELECT * FROM Account WHERE email = ? and password = ?'
+
+    def __init__(self, name: str, email: str, password: str, is_personal: bool, bio: str=None, phone:str=None, uuid:str=""):
+        # Uuid attached to Accoutn for identification
+        self.uuid = str(uuid1()) if not uuid else uuid
         self.name = name
-        self.uuid = str(uuid1())
         self.email = email
+        self.password = password
+        self.is_personal = is_personal
         self.bio = bio
         self.phone = phone
+    
+    @classmethod
+    def exists(self, email: str) -> bool:
+        ''' check if account exists'''
+        conn = sqlite3.connect(Account.DEFAULT_PATH)
+        with conn:
+            curs = conn.cursor()
+            curs.execute(Account.SQL_SELECT_EMAIL, (email, ))
+            # check if exists
+            data = curs.fetchone()
+            # return if account is there 
+            return not (data is None)
+
+    @classmethod
+    def validate(self, email: str, passwd: str): # Returns object or none
+        ''' return object if pass is match'''
+        conn = sqlite3.connect(Account.DEFAULT_PATH)
+        with conn:
+            curs = conn.cursor()
+            curs.execute(Account.SQL_CHECK_VALID, (email, passwd, ))
+            # check if exists
+            data = curs.fetchone()
+            # return if account is there 
+            return data
+
+    @classmethod
+    def init_from_uuid(cls, uuid: str): # returns object that represents Account
+        """Initializes a new Person from the database, using the uuid"""
+        if not uuid: return None
+        conn = sqlite3.connect(Account.DEFAULT_PATH)
+        with conn:
+            curs = conn.cursor()
+            curs.execute(Person.SQL_SELECT_UUID, (uuid,))
+
+            # check if exists
+            data = curs.fetchone()
+            if not data: return None
+
+            # dispatch to proper initializers
+            if data[4]: return Person.init_from_uid(uuid)
+            else:       return Organization.init_from_uid(uuid)
 
     def create_post(self, title: str, description: str, location: str,
-            skill_set: List[str], num_volunteers: int, is_request: bool, tags: List[str] = None):
+            skill_set: List[str], num_volunteers: int, is_request: bool, tags: List[str] = None) -> None:
         ''' Creates post in DB at attaches it to user account '''
         new_post = Post(title, description, location, skill_set, num_volunteers, is_request, self.uuid, tags)
         new_post.insert_into_db()
-    
 
-    def get_name(self):
+    def get_name(self) -> str:
         ''' gets user name '''
         return self.name
 
-
-    def set_name(self, new_name):
+    def set_name(self, new_name: str) -> None:
         ''' sets user name '''
         self.name = new_name
 
-
-    def get_uuid(self):
+    def get_uuid(self) -> str:
         return self.uuid
 
-
-    def get_email(self):
+    def get_email(self) -> str:
         ''' gets user email '''
         return self.email
 
-
-    def set_email(self, new_email):
+    def set_email(self, new_email: str) -> None:
         ''' sets user email '''
         self.email = new_email
 
-
-    def get_phone(self):
+    def get_phone(self) -> str:
         ''' gets user phone '''
         return self.phone
 
-
-    def set_phone(self, new_phone):
+    def set_phone(self, new_phone: str) -> None:
         ''' sets user phone '''
         self.phone = new_phone
 
-
-    def get_bio(self):
+    def get_bio(self) -> str:
         return self.bio
 
-
-    def set_bio(self, new_bio):
+    def set_bio(self, new_bio: str) -> None:
         self.bio = new_bio
 
 
@@ -72,57 +120,52 @@ class Person(Account):
     DEFAULT_PATH = os.path.expanduser(DATABASE_FILE)
 
     SQL_SELECT_UUID = 'SELECT * FROM Person WHERE uuid = ?'
-    SQL_INSERT_PERSON = '''INSERT INTO Person VALUES(?, ?, ?, ?, ?, ?, ?)'''
-    SQL_UPDATE_PERSON = '''UPDATE Person SET name=?, dob=?, bio=?, email=?, phone=?, skills=? WHERE uuid=?'''
+    SQL_INSERT_PERSON = '''INSERT INTO Person VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)'''
+    SQL_UPDATE_PERSON = '''UPDATE Person SET name=?, dob=?, bio=?, email=?, phone=?, skills=?, password=? WHERE uuid=?'''
     SQL_DELETE_PERSON = 'DELETE FROM Person WHERE uuid = ?'
 
-
-    def __init__(self, name, dob, bio=None, email=None, phone=None, skills=None):
-        super(Person, self).__init__(name, bio, email, phone)
+    def __init__(self, name: str, dob: str, email: str, password: str, bio: str=None, phone: str=None, skills: List[str]=None, uuid: str=""):
+        super(Person, self).__init__(name, email, password, True, bio, email, phone, uuid)
         self.dob = dob
         self.skills = skills
 
-
-    def get_dob(self):
+    def get_dob(self) -> str:
         return self.dob
 
-
-    def get_skills(self):
+    def get_skills(self) -> List[str]:
         return self.skills
 
-
-    def set_skills(self, new_skills):
+    def set_skills(self, new_skills: List[str]) -> None:
         self.skills = new_skills
 
-
-    def insert_into_db(self):
+    def insert_into_db(self) -> None:
         skills = ','.join([skill for skill in self.skills]) if self.skills else self.skills
-        conn = sqlite3.connect(Organization.DEFAULT_PATH)
+        conn = sqlite3.connect(Person.DEFAULT_PATH)
         with conn:
             curs = conn.cursor()
-            ins_tuple = (self.uuid, self.email, self.phone, self.name, self.bio, self.dob, skills)
+            ins_tuple = (self.uuid, self.email, self.password, self.phone, self.name, self.bio, self.dob, skills)
             curs.execute(Person.SQL_INSERT_PERSON, ins_tuple)
 
 
-    def update_into_db(self):
+    def update_into_db(self) -> None:
         skills = ','.join([skill for skill in self.skills]) if self.skills else self.skills
-        conn = sqlite3.connect(Organization.DEFAULT_PATH)
+        conn = sqlite3.connect(Person.DEFAULT_PATH)
         with conn:
             curs = conn.cursor()
-            ins_tuple = (self.email, self.phone, self.name, self.bio, self.dob, skills, self.uuid)
+            ins_tuple = (self.email, self.phone, self.name, self.bio, self.dob, skills, self.uuid, self.password)
             curs.execute(Person.SQL_UPDATE_PERSON, ins_tuple)
 
     @classmethod
-    def del_from_db(cls, uuid):
+    def del_from_db(cls, uuid: str) -> None:
         uuid = str(uuid)
-        conn = sqlite3.connect(Organization.DEFAULT_PATH)
+        conn = sqlite3.connect(Person.DEFAULT_PATH)
         with conn:
             curs = conn.cursor()
             curs.execute(Person.SQL_DELETE_PERSON, (uuid,))
 
 
     @classmethod
-    def init_from_uid(cls, uuid=""):
+    def init_from_uid(cls, uuid: str="") -> None:
         """Initializes a new Person from the database, using the uuid"""
         if not uuid: return None
 
@@ -132,7 +175,7 @@ class Person(Account):
             curs.execute(Person.SQL_SELECT_UUID, (uuid,))
             data = curs.fetchone()
             if not data: return None
-            return Person(data[0], data[1], data[2], data[3], data[4], data[5])
+            return Person(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7])
 
 
 class Organization(Account):
@@ -145,9 +188,8 @@ class Organization(Account):
     SQL_UPDATE_ORG = 'UPDATE Organization SET email=?, phone=?, name=?, bio=?, industry=? WHERE uuid=?'
     SQL_DELETE_ORG = 'DELETE FROM Organization WHERE uuid = ?'
 
-
-    def __init__(self, industry, name, bio=None, email=None, phone=None):
-        super(Organization, self).__init__(name, bio, email, phone)
+    def __init__(self, industry, name, email, password, bio=None, phone=None, uuid: str=""):
+        super(Organization, self).__init__(name, email, password, bio, phone, uuid)
         self.industry = industry
 
 
@@ -197,7 +239,6 @@ class Organization(Account):
             if not data: return None
             return Organization(data[0], data[1], data[2], data[3], data[4])
 
-
 if __name__ == '__main__':
     conn = sqlite3.connect(DATABASE_FILE)
     with conn:
@@ -205,10 +246,11 @@ if __name__ == '__main__':
         SQL_CREATE_PERSON_TABLE = '''CREATE TABLE IF NOT EXISTS Organization(
                     uuid VARCHAR(100) PRIMARY KEY,
                     email VARCHAR(100),
+                    password VARCHAR(100),
                     phone CHAR(10),
                     name VARCHAR(100),
                     bio TEXT,
+                    dob DATE,
                     industry VARCHAR(100)
                 )'''
-
         curs.execute(SQL_CREATE_PERSON_TABLE)
