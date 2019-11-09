90e06b347cbd42c32eb999f1d853249a7a770d11
sambattalio <sambattalio99@gmail.com>
2019-11-08 23:41:03 -0500

    token noah pls be gentle

---
+++ backend/token.py
@@ -0,0 +1,37 @@
+# Token -> user uuid database
+
+import sqlite3
+
+from time import time
+
+class TokenTable:
+
+    SQL_CREATE_TABLE = '''create TABLE if not exist token (
+                            token varchar(100),
+                            uuid varchar(100),
+                            expire_time int,
+                            Primary Key(token)
+                        )'''
+
+    TABLE_PATH = 'tokens.db'
+
+    MAX_TTL = 69
+
+    def __init__(self):
+        ''' Initialize SQL Table '''
+        self.conn = sqlite3.connect(self.TABLE_PATH)
+
+        with self.conn:
+            curs = self.conn.cursor()
+            curs.execute(self.SQL_CREATE_TABLE)
+
+    def _get_epoch_time(self):
+        ''' returns current epoch time '''
+        return int(time.time())
+
+    def validate(self, uid, token_id):
+        ''' Checks if userid has token and the token is valid '''
+        
+        
+    def create(self, uid):
+        ''' Create token in table '''
