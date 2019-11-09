73dcbcb452c7ed58f18d884b740840129c95e407
ginglis <ginglis05@gmail.com>
2019-11-09 00:20:33 -0500

    added create org

--- backend/account.py
+++ backend/account.py
@@ -4,6 +4,7 @@ from uuid import uuid1
 import os
 import sqlite3
 
+
 class Account():
 
     def __init__(self, name, bio=None, email=None, phone=None):
@@ -13,38 +14,48 @@ class Account():
         self.bio = bio
         self.phone = phone
 
+
     def get_name(self):
         ''' gets user name '''
         return self.name
+
 
     def set_name(self, new_name):
         ''' sets user name '''
         self.name = new_name
 
+
     def get_uuid(self):
         return self.uuid
+
 
     def get_email(self):
         ''' gets user email '''
         return self.email
 
+
     def set_email(self, new_email):
         ''' sets user email '''
         self.email = new_email
+
 
     def get_phone(self):
         ''' gets user phone '''
         return self.phone
 
+
     def set_phone(self, new_phone):
         ''' sets user phone '''
         self.phone = new_phone
 
+
     def get_bio(self):
         return self.bio
 
+
     def set_bio(self, new_bio):
         self.bio = new_bio
+
 
 class Person(Account):
 
@@ -52,19 +63,24 @@ class Person(Account):
 
     SQL_SELECT_UUID = 'SELECT * FROM Person WHERE uuid = ?'
 
+
     def __init__(self, name, dob, bio=None, email=None, phone=None, skills=None):
         super(Person, self).__init__(name, bio, email, phone)
         self.dob = dob
         self.skills = skills
 
+
     def get_dob(self):
         return self.dob
+
 
     def get_skills(self):
         return self.skills
 
+
     def set_skills(self, new_skills):
         self.skills = new_skills
+
 
     @classmethod
     def init_from_uid(cls, uuid=""):
@@ -79,30 +95,35 @@ class Person(Account):
             if not data: return None
             return Person(data[0], data[1], data[2], data[3], data[4], data[5])
 
+
 class Organization(Account):
 
     DEFAULT_PATH = os.path.expanduser('example.db')
 
     SQL_SELECT_UUID = 'SELECT * FROM Organization WHERE uuid = ?'
+    SQL_INSERT_ORG = '''INSERT INTO Organization VALUES(?, ?, ?, ?, ?, ?)'''
+
 
     def __init__(self, industry, name, bio=None, email=None, phone=None):
         super(Organization, self).__init__(name, bio, email, phone)
         self.industry = industry
 
+
     def get_industry(self):
         return self.industry
+
 
     def set_industry(self, new_industry):
         self.industry = new_industry
 
-    def insert_into_db(self):
-        SQL_INSERT_ORG = '''INSERT INTO Organization VALUES(?, ?, ?, ?, ?)'''
 
+    def insert_into_db(self):
         conn = sqlite3.connect(Organization.DEFAULT_PATH)
         with conn:
             curs = conn.cursor()
             ins_tuple = (self.uuid, self.email, self.phone, self.name, self.bio, self.industry)
             curs.execute(Organization.SQL_INSERT_ORG, ins_tuple)
+
 
     @classmethod
     def init_from_uid(cls, uuid=""):
@@ -121,7 +142,7 @@ class Organization(Account):
 if __name__ == '__main__':
     conn = sqlite3.connect('example.db')
     with conn:
-        conn.execute("DROP TABLE Organization")
+        curs = conn.cursor()
         SQL_CREATE_ORG_TABLE = '''CREATE TABLE IF NOT EXISTS Organization(
                     uuid VARCHAR(100) PRIMARY KEY,
                     email VARCHAR(100),
@@ -131,11 +152,10 @@ if __name__ == '__main__':
                     industry VARCHAR(100)
                 )'''
 
-        conn.execute(SQL_CREATE_ORG_TABLE)
+        curs.execute(SQL_CREATE_ORG_TABLE)
 
-        SQL_INSERT_ORG = 'INSERT INTO Organization VALUES("123", "ginglisyo", "1234567890", "garvingarvin", "pair programmer", "nothing", "fuck")'
-        conn.execute(SQL_INSERT_ORG)
-        conn.commit()
+        o = Organization("computing", "amzn")
+        o.insert_into_db()
 
-        o = Organization.init_from_uid("123")
-        print(o.name, o.bio, o.industry)
+        for row in curs.execute('SELECT * FROM Organization'):
+            print(row)
