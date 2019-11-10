5027550b1ca78af32f56da4c67f3a6d84e67858c
ginglis <ginglis05@gmail.com>
2019-11-09 20:03:06 -0500

    sam error part 2

--- backend/post.py
+++ backend/post.py
@@ -163,7 +163,7 @@ class Post:
             curs = conn.cursor()
             # create input tuple to match SQL_UPDATE_POST's ? operators
             data = (self.title, self.description, self.location, ','.join(self.skill_set), self.num_volunteers, self.is_request,
-                    self.user_id, ','.join(self.tags) if self.tags else None, ','.join(self.volunteers) if self.volunteers else None, self.uuid)
+                    self.user_id, ','.join(self.tags) if self.tags else None, ','.join(self.volunteers) if self.volunteers else None, self.date, self.length, self.uuid)
             # perform sql update
             curs.execute(Post.SQL_UPDATE_POST, data)
 
